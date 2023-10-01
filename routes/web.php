d89b25a15c6126973d7835489b97d4e252501045
Alikaram <alikaramniyabadreh98@gmail.com>
2023-10-01 15:29:45 +0330

    optimize profile route

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')
+    ->name('profile')
+    ->controller(ProfileController::class)
+    ->group(function () {
+    Route::get('/profile', 'edit')->name('edit');
+    Route::patch('/profile', 'update')->name('update');
+    Route::delete('/profile', 'destroy')->name('destroy');
 });
 
 require __DIR__.'/auth.php';
